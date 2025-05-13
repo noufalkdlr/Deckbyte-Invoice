ef048fae3bed13643c38582db37ae32e52835471
noufalkakdlr <noufalkakdlr@gmail.com>
2025-05-13 10:57:11 +0530

    aligment

--- invoice/src/Invoice.jsx
+++ invoice/src/Invoice.jsx
@@ -1,6 +1,5 @@
 import jsPDF from "jspdf";
 import { useRef } from "react";
-import { useLocation } from "react-router-dom";
 
 const Invoice = ({ data, invoiceNo, billTo, invoiceDate }) => {
   const invoicRef = useRef();
@@ -15,39 +14,46 @@ const Invoice = ({ data, invoiceNo, billTo, invoiceDate }) => {
       x: 0,
       y: 0,
       html2canvas: {
-        scale: 0.3,
+        scale: 0.2,
         useCORS: true,
       },
     });
   };
   return (
     <>
-      <div ref={invoicRef} className="bg-slate-400 max-w-screen-sm">
-        <p className="text-8xl">invoice</p>
-        <p>invoic no: {invoiceNo}</p>
-        <p>date:{invoiceDate}</p>
+      <div className="flex justify-center">
+        <div
+          ref={invoicRef}
+          className="bg-slate-400 min-w-[1049px]  min-h-[1485px] px-16"
+        >
+            <p className="text-8xl">invoice</p>
+            <p>invoic no: {invoiceNo}</p>
+            <p>date:{invoiceDate}</p>
 
-        <div>
-          <p>{billTo.client}</p>
-          <p>{billTo.address}</p>
-          <p>{billTo.phone}</p>
+            <div>
+              <p>{billTo.client}</p>
+              <p>{billTo.address}</p>
+              <p>{billTo.phone}</p>
+            </div>
+            {data.map((item, index) => (
+              <div key={index}>
+                <p>Item name {item.itemName} </p>
+                <p>Item QTY {item.itemQTY} </p>
+                <p>Item Price {item.itemPrice} </p>
+                <p>Total: {Number(item.itemPrice) * Number(item.itemQTY)}</p>
+              </div>
+            ))}
+            <p>
+              Total Amount:{" "}
+              {data.reduce(
+                (acc, item) =>
+                  acc + Number(item.itemPrice) * Number(item.itemQTY),
+                0
+              )}
+            </p>
         </div>
-        {data.map((item, index) => (
-          <div key={index}>
-            <p>Item name {item.itemName} </p>
-            <p>Item QTY {item.itemQTY} </p>
-            <p>Item Price {item.itemPrice} </p>
-            <p>Total: {Number(item.itemPrice) * Number(item.itemQTY)}</p>
-          </div>
-        ))}
-        <p>
-          Total Amount:{" "}
-          {data.reduce(
-            (acc, item) => acc + Number(item.itemPrice) * Number(item.itemQTY),
-            0
-          )}
-        </p>
       </div>
+
       <button onClick={handleDownload}>Download</button>
     </>
   );
