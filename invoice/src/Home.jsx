646bff28d9515f0e167cd084bc6ef03154876437
noufalkdlr <noufalkakdlr@gmail.com>
2025-05-18 08:06:40 +0530

    calculation fix

--- invoice/src/Home.jsx
+++ invoice/src/Home.jsx
@@ -2,10 +2,13 @@ import React, { use, useState } from "react";
 import Invoice from "./Invoice";
 
 const Home = () => {
-  const [data, setData] = useState([{ itemName: "", itemQTY: "", itemPrice: "" }]);
+  const [data, setData] = useState([
+    { itemName: "", itemQTY: "", itemPrice: "" },
+  ]);
   const [invoiceNo, setInvoiceNo] = useState("");
   const [invoiceDate, setInvoiceDate] = useState("");
   const [billTo, setBillTo] = useState({ client: "", address: "", phone: "" });
+  const [advancePaid, setAdvancePaid] = useState("");
 
   const handleDataChange = (index, e) => {
     const { name, value } = e.target;
@@ -30,83 +33,96 @@ const Home = () => {
     setData([...data, { itemName: "", itemPrice: "" }]);
   };
 
-  const handleRemoveItem = (index) =>{
-    if(data.length>1){
+  const handleRemoveItem = (index) => {
+    if (data.length > 1) {
       const updateData = data.filter((_, i) => i !== index);
-      setData(updateData)
-
+      setData(updateData);
     }
-  }
+  };
+
+  const handleAdvancePaid = (e) => {
+    setAdvancePaid(e.target.value);
+  };
 
   return (
     <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="date"
-          name="invoiceDate"
-          value={invoiceDate}
-          onChange={hadleInvoiceDate}
-        />
-        <input
-          type="text"
-          name="client"
-          value={billTo.client}
-          onChange={handleBillTo}
-          placeholder="Client Name"
-        />
-        <input
-          type="text"
-          name="address"
-          value={billTo.address}
-          onChange={handleBillTo}
-          placeholder="Address"
-        />
-        <input
-          type="text"
-          name="phone"
-          value={billTo.phone}
-          onChange={handleBillTo}
-          placeholder="Phone NO"
-        />
-        {data.map((item, index) => (
-          <div key={index}>
-            <label>Item {index + 1}</label>
-            <input
-              type="text"
-              name="itemName"
-              value={item.itemName}
-              onChange={(e) => handleDataChange(index, e)}
-              placeholder="Name"
-            />
-            <input
-              type="number"
-              name="itemQTY"
-              value={item.itemQTY}
-              onChange={(e) => handleDataChange(index, e)}
-              placeholder=" QTY"
-            />
-            <input
-              type="number"
-              name="itemPrice"
-              value={item.itemPrice}
-              onChange={(e) => handleDataChange(index, e)}
-              placeholder=" Price"
-            />
-              <button type="button" onClick={() => handleRemoveItem(index) }>
+      <div className="flex justify-center mx-auto py-10 w-[50%]">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            name="client"
+            value={billTo.client}
+            onChange={handleBillTo}
+            placeholder="Client Name"
+          />
+          <input
+            type="date"
+            name="invoiceDate"
+            value={invoiceDate}
+            onChange={hadleInvoiceDate}
+          />
+          <textarea
+            name="address"
+            value={billTo.address}
+            onChange={handleBillTo}
+            placeholder="Address"
+            rows={3}
+          />
+          <input
+            type="text"
+            name="phone"
+            value={billTo.phone}
+            onChange={handleBillTo}
+            placeholder="Phone NO"
+          />
+          {data.map((item, index) => (
+            <div key={index}>
+              <label>Item {index + 1}</label>
+              <input
+                type="text"
+                name="itemName"
+                value={item.itemName}
+                onChange={(e) => handleDataChange(index, e)}
+                placeholder="Name"
+              />
+              <input
+                type="number"
+                name="itemQTY"
+                value={item.itemQTY}
+                onChange={(e) => handleDataChange(index, e)}
+                placeholder=" QTY"
+              />
+              <input
+                type="number"
+                name="itemPrice"
+                value={item.itemPrice}
+                onChange={(e) => handleDataChange(index, e)}
+                placeholder=" Price"
+              />
+
+              <button type="button" onClick={() => handleRemoveItem(index)}>
                 Remov Item
               </button>
-          </div>
-          
-        ))}
-        <button type="button" onClick={handleAddItem}>
-          ➕ Add Item
-        </button>
-      </form>
+            </div>
+          ))}
+          <button type="button" onClick={handleAddItem}>
+            ➕ Add Item
+          </button>
+
+          <input
+            type="number"
+            value={advancePaid}
+            onChange={handleAdvancePaid}
+            placeholder="Advance paid amount"
+          />
+        </form>
+      </div>
       <Invoice
         data={data}
         invoiceNo={invoiceNo}
         billTo={billTo}
         invoiceDate={invoiceDate}
+        advancePaid ={advancePaid}
       />
     </>
   );
--- invoice/vite.config.js
+++ invoice/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Accept connections from any IP
+    port: 5173       // Or use a custom port if you like
+  }
 })
