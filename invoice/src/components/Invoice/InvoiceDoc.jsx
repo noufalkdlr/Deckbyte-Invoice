640b0c233f4079b25fd4925df7ecefe59898f9b4
noufalkakdlr <noufalkakdlr@gmail.com>
2025-08-24 17:09:42 +0530

    fix buffer

--- invoice/src/components/Invoice/InvoiceDoc.jsx
+++ invoice/src/components/Invoice/InvoiceDoc.jsx
@@ -1,12 +1,6 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import {
-  Page,
-  Text,
-  View,
-  Document,
-  Image,
-} from "@react-pdf/renderer";
+import { Page, Text, View, Document, Image } from "@react-pdf/renderer";
 import { styles } from "./styles";
 
 import logo from "../../assets/DeckbyteLogo.png";
@@ -20,7 +14,7 @@ const InvoiceDoc = ({
   advanceAmount,
   finalPayment,
 }) => {
-    const firstInvoice = invoiceType === "Invoice";
+  const firstInvoice = invoiceType === "Invoice";
   const firstReceipt = invoiceType === "Receipt";
   const finalInvoice = invoiceType === "Final Invoice";
   const finalReceipt = invoiceType === "Final Receipt";
@@ -42,55 +36,173 @@ const InvoiceDoc = ({
     0
   );
 
-    return  (
-      <Document>
-        <Page size="A4" style={styles.page}>
-          <View style={styles.main}>
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.main}>
+          <View>
             <View>
-              <View>
-                <Image src={logo} style={styles.logo} />
-              </View>
-            </View>
-            <View style={styles.CNameInvoice}>
-              <View>
-                <Text style={styles.CName}>DECKBYTE DIGITAL SOLUTIONS</Text>
-                <View>
-                  <Text style={{ marginBottom: 5 }}>
-                    Kodungallur, Thrissur, Kerala, India
-                  </Text>
-                  <Text>+91 95628 66814 | www.deckbyte.in</Text>
-                </View>
-              </View>
-              <View style={styles.InvoiceNoDate}>
-                <Text style={styles.Invoice}>INVOICE</Text>
-                <Text style={{ marginBottom: 4 }}>
-                  Invoice No.: {invoiceNumber}
-                </Text>
-                <Text>
-                  Date:{" "}
-                  {invoiceDate ||
-                    new Date().toLocaleDateString("en-GB").replaceAll("/", "-")}
-                </Text>
-              </View>
-            </View>
-            <View>
-              <Text>BILL TO:</Text>
-              <Text>{billTo.client || "Name"}</Text>
-              {billTo.address ? (
-                billTo.address
-                  .split("\n")
-                  .map((line, i) => <Text key={i}>{line}</Text>)
-              ) : (
-                <Text>Address</Text>
-              )}
-              <Text>{billTo.phone}</Text>
+              <Image src={logo} style={styles.logo} />
             </View>
           </View>
-        </Page>
-      </Document>
-  )
-}
+          <View style={styles.CNameInvoice}>
+            <View>
+              <Text style={styles.CName}>DECKBYTE DIGITAL SOLUTIONS</Text>
+              <View>
+                <Text style={{ marginBottom: 5 }}>
+                  Kodungallur, Thrissur, Kerala, India
+                </Text>
+                <Text>+91 95628 66814 | www.deckbyte.in</Text>
+              </View>
+            </View>
+            <View style={styles.InvoiceNoDate}>
+              {firstInvoice || finalInvoice ? (
+                <Text style={styles.InvoiceType}>INVOICE</Text>
+              ) : (
+                <Text style={styles.InvoiceType}>RECEIPT</Text>
+              )}
+              <Text style={{ marginBottom: 4 }}>
+                Invoice No.: {invoiceNumber}
+              </Text>
+              <Text>
+                Date:{" "}
+                {invoiceDate ||
+                  new Date().toLocaleDateString("en-GB").replaceAll("/", "-")}
+              </Text>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.BillTo}>BILL TO:</Text>
+            <Text style={{ marginBottom: 8 }}>
+              {billTo.client || "Tamara Resto Bar"}
+            </Text>
+            <Text style={{ marginBottom: 8 }}>
+              {billTo.address ||
+                `Thrissur, Kodungallur ${"\n"}Kerala, India, 680666`}
+            </Text>
+            <Text>{billTo.phone || "+91 974646 9319"}</Text>
+          </View>
+          <View style={styles.Table}>
+            {/* Table Header */}
+            <View style={styles.TableHeader}>
+              <Text style={{ flex: 4 }}>DESCRIPTION</Text>
+              <Text style={{ flex: 1, textAlign: "center" }}>QTY</Text>
+              <Text style={{ flex: 1, textAlign: "center" }}>PRICE</Text>
+              <Text style={{ flex: 1, textAlign: "right" }}>TOTAL</Text>
+            </View>
 
+            {/* Table Body */}
+            <View style={styles.TableBody}>
+              {data.map((item, index) => (
+                <View key={index} style={styles.TableItems}>
+                  <Text style={{ flex: 4 }}>{item.itemName || "logo"}</Text>
+                  <Text style={{ flex: 1, textAlign: "center" }}>
+                    {item.itemQTY || 2}
+                  </Text>
+                  <Text style={{ flex: 1, textAlign: "center" }}>
+                    {item.itemPrice || 3000}
+                  </Text>
+                  <Text style={{ flex: 1, textAlign: "right" }}>
+                    {Number(item.itemPrice) * Number(item.itemQTY)}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          </View>
+          <View style={styles.summaryBox}>
+            {firstInvoice ? (
+              
+                <View style={styles.summaryRow}>
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingBottom:8,}}>
+                    <Text>Subtotal</Text>
+                    <Text style={{textAlign:"right", }}>₹{totalAmount}</Text>
+                  </View>
 
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingVertical:8,}}>
+                    <Text>Advance Amount</Text>
+                    <Text style={{textAlign:"right", }}>₹{advanceAmount || 0}</Text>
+                  </View>
 
-export default InvoiceDoc
+                  <View style={{flexDirection:"row", justifyContent:"space-between", paddingTop:8,}}>
+                    <Text>Balance Due</Text>
+                    <Text style={{textAlign:"right", fontFamily:"Work Sans", fontSize: 14, fontWeight: "600", color:"#000", }}>₹{totalAmount - advanceAmount}/-</Text>
+                  </View>
+                </View>
+              
+            ) : null}
+             {firstReceipt ? (
+              
+                <View style={styles.summaryRow}>
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingBottom:8,}}>
+                    <Text>Total Invoice Amount</Text>
+                    <Text style={{textAlign:"right", }}>₹{totalAmount}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingVertical:8,}}>
+                    <Text>Advance Received</Text>
+                    <Text style={{textAlign:"right", }}>₹{advancePaid || 0}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", paddingTop:8,}}>
+                    <Text>Balance Due</Text>
+                    <Text style={{textAlign:"right", fontFamily:"Work Sans", fontSize: 14, fontWeight: "600", color:"#000", }}>₹{totalAmount - advancePaid}/-</Text>
+                  </View>
+                </View>
+              
+            ) : null}
+            {finalInvoice ? (
+              
+                <View style={styles.summaryRow}>
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingBottom:8,}}>
+                    <Text>Total Invoice Amount</Text>
+                    <Text style={{textAlign:"right", }}>₹{totalAmount}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingVertical:8,}}>
+                    <Text>Advance Paid</Text>
+                    <Text style={{textAlign:"right", }}>₹{advancePaid || 0}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", paddingTop:8,}}>
+                    <Text>Balance Due</Text>
+                    <Text style={{textAlign:"right", fontFamily:"Work Sans", fontSize: 14, fontWeight: "600", color:"#000", }}>₹{totalAmount - advancePaid}/-</Text>
+                  </View>
+                </View>
+              
+            ) : null}
+            {finalReceipt ? (
+              
+                <View style={styles.summaryRow}>
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingBottom:8,}}>
+                    <Text>Total Invoice Amount</Text>
+                    <Text style={{textAlign:"right", }}>₹{totalAmount}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingVertical:8,}}>
+                    <Text>Advance Paid</Text>
+                    <Text style={{textAlign:"right", }}>₹{advancePaid || 0}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", borderBottom:.25, paddingVertical:8,}}>
+                    <Text>Final Payment Received</Text>
+                    <Text style={{textAlign:"right", }}>₹{finalPayment || 0}</Text>
+                  </View>
+
+                  <View style={{flexDirection:"row", justifyContent:"space-between", paddingTop:8,}}>
+                    <Text>Balance Due</Text>
+                    <Text style={{textAlign:"right", fontFamily:"Work Sans", fontSize: 14, fontWeight: "600", color:"#000", }}>₹{totalAmount - finalPayment - advancePaid}/-</Text>
+                  </View>
+                </View>
+              
+            ) : null}
+          </View>
+          <View style={styles.bottom}>
+            <View style={styles.bottomLine} />
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+export default InvoiceDoc;
--- invoice/src/components/Invoice/styles.js
+++ invoice/src/components/Invoice/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet, Font } from "@react-pdf/renderer";
 import MontserratSemiBold from "../../assets/fonts/Montserrat-SemiBold.ttf";
 import MontserratBold from "../../assets/fonts/Montserrat-Bold.ttf";
 import WorkSansRegular from "../../assets/fonts/WorkSans-Regular.ttf";
+import WorkSansSemiBold from "../../assets/fonts/WorkSans-SemiBold.ttf";
 
 Font.register({
   family: "Montserrat",
@@ -14,7 +15,10 @@ Font.register({
 
 Font.register({
   family: "Work Sans",
-  fonts: [{ src: WorkSansRegular, fontWeight: "normal" }],
+  fonts: [
+    { src: WorkSansRegular, fontWeight: "normal" },
+    { src: WorkSansSemiBold, fontWeight: "600" }
+  ],
 });
 
 export const styles = StyleSheet.create({
@@ -32,14 +36,14 @@ export const styles = StyleSheet.create({
     flexGrow: 1,
   },
   main: {
-    backgroundColor: "#d8d8d8",
+    // backgroundColor: "#d8d8d8",
     marginHorizontal: 38,
     marginVertical: 35,
     flex: 1,
   },
   logo: {
     width: 190,
-    marginBottom: 28,
+    marginBottom: 27,
   },
   CName: {
     fontFamily: "Montserrat",
@@ -52,6 +56,15 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "flex-start",
+    marginBottom: 27,
+  },
+
+  BillTo: {
+    fontFamily: "Montserrat",
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#000000",
+    marginBottom: 7,
   },
 
   InvoiceNoDate: {
@@ -59,11 +72,58 @@ export const styles = StyleSheet.create({
     alignItems: "flex-end",
   },
 
-  Invoice: {
+  InvoiceType: {
     fontFamily: "Montserrat",
     fontSize: 18,
     fontWeight: "bold",
-    color:"#cc0e4e",
+    color: "#cc0e4e",
     marginBottom: 3,
+  },
+  Table: {
+    marginTop: 27,
+  },
+  TableHeader: {
+    flexDirection: "row",
+    fontFamily: "Montserrat",
+    fontSize: 12,
+    fontWeight: "bold",
+    color: "#000000",
+    paddingBottom: 8,
+  },
+  TableBody: {
+    borderTop: 0.25,
+    borderBottom: 0.25,
+    borderColor: "#000",
+    paddingTop: 14,
+    paddingBottom: 6,
+  },
+  TableItems: {
+    flexDirection: "row",
+    marginBottom: 8,
+  },
+    summaryBox: {
+      marginTop:15,
+      flexDirection:"row",
+      justifyContent:"flex-end",
+  },
+    summaryRow: {
+    width:200,
+  },
+
+
+
+
+
+  bottom: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  bottomLine: {
+    height: 85,
+    // backgroundColor: "#cc0e4e",
+    borderTop: 0.25,
+    borderColor: "#000",
   },
 });
