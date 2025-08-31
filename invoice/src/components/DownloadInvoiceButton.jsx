cf6a9b39d0b5479655542f74101c3370b09aa7cd
noufalkakdlr <noufalkakdlr@gmail.com>
2025-08-31 10:38:38 +0530

    clean code

--- invoice/src/components/DownloadInvoiceButton.jsx
+++ invoice/src/components/DownloadInvoiceButton.jsx
@@ -1,27 +1,15 @@
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import Button from "@mui/material/Button";
-import InvoiceDoc from "./Invoice/InvoiceDoc";
-import ReceiptDoc from "./Invoice/ReceiptDoc";
-import FinalInvoiceDoc from "./Invoice/FinalInvoiceDoc";
-import FinalReceiptDoc from "./Invoice/FinalReceiptDoc";
+
+import InvoiceReceiptDesign from "./Invoice/InvoiceReceiptDesign";
 
 export default function DownloadInvoiceButton(props) {
-  const { invoiceType } = props;
 
-  const document =
-    invoiceType === "Invoice" ? (
-      <InvoiceDoc {...props} />
-    ) : invoiceType === "Receipt" ? (
-      <ReceiptDoc {...props} />
-    ) : invoiceType === "Final Invoice" ? (
-      <FinalInvoiceDoc {...props} />
-    ) : invoiceType === "Final Receipt" ? (
-      <FinalReceiptDoc {...props} />
-    ) : null;
+
 
   return (
     <PDFDownloadLink
-      document={document}
+      document={<InvoiceReceiptDesign {...props} />}
       fileName={`${props.billTo.client || "INV"}_${
         props.invoiceDate || new Date().toLocaleDateString()
       }.pdf`}
--- invoice/src/pages/Preview.jsx
+++ invoice/src/pages/Preview.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Button from "@mui/material/Button";
-// import InvoicePreview from "../components/Invoice/InvoicePreview";
 import DownloadInvoiceButton from "../components/DownloadInvoiceButton";
 import { useNavigate } from "react-router-dom";
-// import { PDFViewer } from "@react-pdf/renderer";
+
 import { BlobProvider } from "@react-pdf/renderer";
 
-import InvoiceDoc from "../components/Invoice/InvoiceDoc";
-import ReceiptDoc from "../components/Invoice/ReceiptDoc";
-import FinalInvoiceDoc from "../components/Invoice/FinalInvoiceDoc";
-import FinalReceiptDoc from "../components/Invoice/FinalReceiptDoc";
+import InvoiceReceiptDesign from "../components/Invoice/InvoiceReceiptDesign";
 
 const Preview = () => {
   const navigate = useNavigate();
@@ -30,23 +26,10 @@ const Preview = () => {
     <div className="flex flex-col items-center ]">
       {state ? (
         <div className="w-full">
-          {invoiceType === "Invoice" && (
             <div className="w-screen h-[1200px]">
-              {/* <PDFViewer width="100%" height="100%">
-                <InvoiceDoc
-                  data={data || []}
-                  billTo={billTo || {}}
-                  invoiceDate={invoiceDate}
-                  advancePaid={advancePaid}
-                  invoiceType={invoiceType}
-                  advanceAmount={advanceAmount}
-                  finalPayment={finalPayment}
-                  invoiceNumber={invoiceNumber}
-                />
-              </PDFViewer> */}
               <BlobProvider
                 document={
-                  <InvoiceDoc
+                  <InvoiceReceiptDesign
                     data={data || []}
                     billTo={billTo || {}}
                     invoiceDate={invoiceDate}
@@ -71,97 +54,6 @@ const Preview = () => {
                 }
               </BlobProvider>
             </div>
-          )}
-          {invoiceType === "Receipt" && (
-            <div className="w-screen h-[1200px]">
-              <BlobProvider
-                document={
-                  <ReceiptDoc
-                    data={data || []}
-                    billTo={billTo || {}}
-                    invoiceDate={invoiceDate}
-                    advancePaid={advancePaid}
-                    invoiceType={invoiceType}
-                    advanceAmount={advanceAmount}
-                    finalPayment={finalPayment}
-                    invoiceNumber={invoiceNumber}
-                  />
-                }
-              >
-                {({ url }) =>
-                  url ? (
-                    <iframe
-                      src={url}
-                      className="w-full h-[1200px] border-0"
-                      style={{ background: "white" }}
-                    />
-                  ) : (
-                    <p>Loading preview...</p>
-                  )
-                }
-              </BlobProvider>
-            </div>
-          )}
-          {invoiceType === "Final Invoice" && (
-            <div className="w-screen h-[1200px]">
-              <BlobProvider
-                document={
-                  <FinalInvoiceDoc
-                    data={data || []}
-                    billTo={billTo || {}}
-                    invoiceDate={invoiceDate}
-                    advancePaid={advancePaid}
-                    invoiceType={invoiceType}
-                    advanceAmount={advanceAmount}
-                    finalPayment={finalPayment}
-                    invoiceNumber={invoiceNumber}
-                  />
-                }
-              >
-                {({ url }) =>
-                  url ? (
-                    <iframe
-                      src={url}
-                      className="w-full h-[1200px] border-0"
-                      style={{ background: "white" }}
-                    />
-                  ) : (
-                    <p>Loading preview...</p>
-                  )
-                }
-              </BlobProvider>
-            </div>
-          )}
-          {invoiceType === "Final Receipt" && (
-            <div className="w-screen h-[1200px]">
-              <BlobProvider
-                document={
-                  <FinalReceiptDoc
-                    data={data || []}
-                    billTo={billTo || {}}
-                    invoiceDate={invoiceDate}
-                    advancePaid={advancePaid}
-                    invoiceType={invoiceType}
-                    advanceAmount={advanceAmount}
-                    finalPayment={finalPayment}
-                    invoiceNumber={invoiceNumber}
-                  />
-                }
-              >
-                {({ url }) =>
-                  url ? (
-                    <iframe
-                      src={url}
-                      className="w-full h-[1200px] border-0"
-                      style={{ background: "white" }}
-                    />
-                  ) : (
-                    <p>Loading preview...</p>
-                  )
-                }
-              </BlobProvider>
-            </div>
-          )}
         </div>
       ) : (
         <p>No data passed</p>
