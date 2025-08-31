d1862b8e35f71458ae46dcd609a49a3b323a7189
noufalkakdlr <noufalkakdlr@gmail.com>
2025-08-31 18:42:05 +0530

    add icons

--- invoice/src/main.jsx
+++ invoice/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Buffer } from "buffer";
+import { registerSW } from 'virtual:pwa-register'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+registerSW()
 
 
 window.Buffer = Buffer;
--- invoice/vite.config.js
+++ invoice/vite.config.js
@@ -1,11 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Deckbyte Invoice",
+        short_name: "InvoiceGen",
+        description: "Create and download invoices easily",
+        theme_color: "#1976d2",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
-    host: '0.0.0.0', // Accept connections from any IP
-    port: 5173       // Or use a custom port if you like
-  }
-})
+    host: "0.0.0.0", // Accept connections from any IP
+    port: 5173, // Or use a custom port if you like
+  },
+});
